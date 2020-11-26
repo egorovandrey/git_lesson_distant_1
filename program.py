42f254f77aae29a64bb3c404cb0fef7b77e5bc30
egorov.andrey <egorov.andrey@list.ru>
2020-11-26 17:09:08 +0300

    Добавили четвертый принт

--- program.py
+++ program.py
@@ -2,6 +2,7 @@ def main():
     print('My first git program')
     print('Second line')
     print('Это строка другого разработчика')
+    print('Добавил еще что-то')
 
 
 if __name__ == '__main__':
